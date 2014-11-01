0c3d90e0a32ba990dd69de62c98e2e5d25556aa7
Ashok Menon <amenon94@gmail.com>
2014-11-01 22:11:16 +0000

    Adding dependencies and description to project file.

--- project.clj
+++ project.clj
@@ -1,9 +1,10 @@
 (defproject knn "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :description "An implementation of the kth nearest neighbour, in Clojure.
+               Specialised to R^2, using a quadtree to store the training
+               data."
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/data.priority-map "0.0.5"]]
+
   :main ^:skip-aot knn.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
