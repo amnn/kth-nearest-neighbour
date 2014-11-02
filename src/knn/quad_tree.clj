8254ad77262896cec5f8afe33b04c4c38ebbfdfd
Ashok Menon <amenon94@gmail.com>
2014-11-02 01:21:52 +0000

    Quad Tree: Implementing k nearest neighbours procedure.
    Also fixing instances where the docstring appeared in the wrong
    place in the function (after the formal parameters).

--- src/knn/quad_tree.clj
+++ src/knn/quad_tree.clj
@@ -1,10 +1,12 @@
-(ns knn.quad-tree)
+(ns knn.quad-tree
+  (:require [clojure.data.priority-map :refer :all]))
 
 (defrecord ^:private QuadTree
   [pos label tl tr bl br])
 
-(defn- quad [qt pos]
+(defn- quad
   "Returns the sub-quad the position belongs to."
+  [qt pos]
   (let [[dx dy] (map - pos (:pos qt))
         neg-dx? (neg? dx)
         neg-dy? (neg? dy)]
@@ -12,8 +14,34 @@
       (if neg-dx? :bl :br)
       (if neg-dx? :tl :tr))))
 
-(defn insert [qt [pos label :as plp]]
+(defn- x-nbr
+  "Lateral neighbour of a sub-quad"
+  [sq]
+  (case sq
+    :tl :tr, :tr :tl
+    :bl :br, :br :bl))
+
+(defn- y-nbr
+  "Vertical neighbour of a sub-quad"
+  [sq]
+  (case sq
+    :tl :bl, :tr :br
+    :bl :tl, :br :tr))
+
+(defn- d-nbr
+  "Diagonal neighbour of a sub-quad"
+  [sq]
+  (case sq
+    :tl :br, :tr :bl
+    :bl :tr, :br :tl))
+
+(defn- diag?
+  "Predicate to determine whether two sub-quads are diagonal to each other."
+  [a b] (= a (d-nbr b)))
+
+(defn insert
   "Insert a `pos`, `label` pair into the `qt` quad-tree."
+  [qt [pos label :as plp]]
   (if qt
     (update-in qt [(quad qt pos)]
                insert plp)
@@ -27,3 +55,39 @@
   ([& plps]
    (reduce insert (quad-tree)
            (partition 2 plps))))
+
+(defn- dist
+  "Euclidean distance in R^2 between two points."
+  [a b]
+  (let [[dx dy] (map - a b)]
+    (Math/sqrt (+ (* dx dx)
+                  (* dy dy)))))
+
+(defn- furthest
+  "Returns the distance of the furthest neighbour."
+  [nbrs]
+  (if-let [kvp (->> nbrs rseq first)]
+    (val kvp)
+    0))
+
+(defn k-nn
+  "Returning the k nearest neighbours to `pos` in `qt`, if there are that many
+  such neighbours."
+  [qt [x y :as pos] k]
+  (if qt
+    (let [[cx cy :as pos-qt] (:pos qt)
+          sq   (quad qt pos)
+          dd   (dist pos pos-qt)
+          nbrs (priority-map
+                 (x-nbr sq) (Math/abs (- x cx))
+                 (y-nbr sq) (Math/abs (- y cy))
+                 (d-nbr sq) dd)]
+      (into (priority-map)
+            (take k (-> (reduce (fn [nns [sq* d]]
+                                  (if (< (furthest nns) d) nns
+                                    (->> (k-nn (sq* qt) pos k)
+                                         (into nns) (take k)
+                                         (into (empty nns)))))
+                                (k-nn (sq qt) pos k) nbrs)
+                        (assoc [pos-qt (:label qt)] dd)))))
+    (priority-map)))
