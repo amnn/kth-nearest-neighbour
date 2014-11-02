d5ef3f9fa57b9df9d7fed86e2d90f23828394376
Ashok Menon <amenon94@gmail.com>
2014-11-02 01:37:33 +0000

    Core: Added method for reading samples into memory.

--- src/knn/core.clj
+++ src/knn/core.clj
@@ -1,1 +1,15 @@
-(ns knn.core)
+(ns knn.core
+  (:require [clojure.java.io :refer [reader]]
+            [knn.quad-tree :as qt]))
+
+(defn read-sample
+  "Takes two filenames `pos-fname` and `label-fname` and produces a sequence of
+  position-label pairs."
+  [pos-fname label-fname]
+  (with-open [pos-f (reader pos-fname)
+              lab-f (reader label-fname)]
+    (let [pos-lines (line-seq pos-f)
+          lab-lines (line-seq lab-f)]
+      (doall (map #(vector (read-string (str \[ % \]))
+                           (read-string %2))
+                  pos-lines lab-lines)))))
